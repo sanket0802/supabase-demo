0e59596772310772e92930517e2737b9d594528f
sanket <sanket@gmail.com>
2024-05-10 11:10:24 +0530

    added supabase connections

---
+++ utils/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+export default createClient(
+  'https://sqapoiyrkingyhniuegm.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxYXBvaXlya2luZ3lobml1ZWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyNTU4NjYsImV4cCI6MjAzMDgzMTg2Nn0.So7t7qCsIMZ_cKXjSXlNXs5IVKzOucMkka8EPgiePVg'
+)
+
